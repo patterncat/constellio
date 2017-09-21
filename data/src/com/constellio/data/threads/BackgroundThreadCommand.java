2f331d19926ffc87f7e8993a0e4fec0e5f93671f
dakota.indien <dakota.indien@gmail.com>
2017-09-21 10:17:49 -0400

    removed lock (temp)

--- data/src/com/constellio/data/threads/BackgroundThreadCommand.java
+++ data/src/com/constellio/data/threads/BackgroundThreadCommand.java
@@ -55,7 +55,7 @@ public class BackgroundThreadCommand implements Runnable {
 			}
 		}
 
-		synchronized (configuration.getId().intern()) {
+		//synchronized (configuration.getId().intern()) {
 			try {
 
 				while (!systemStarted.get() && !stopRequested.get()) {
@@ -82,7 +82,7 @@ public class BackgroundThreadCommand implements Runnable {
 			} finally {
 				isRunning = false;
 			}
-		}
+		//}
 
 	}
 
